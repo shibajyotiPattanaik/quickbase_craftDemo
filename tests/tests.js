31d6dc59a15c18a801c15e115d4be406cc4ba1c2
Shiv Pattanaik <shivpattanaik@Shivs-MacBook-Pro.local>
2021-04-15 18:10:05 -0400

    added test_data.json for storing test values

--- tests/tests.js
+++ tests/tests.js
@@ -1,15 +1,17 @@
-// UI Automation Exercise
-// Write a UI end to end user test class (language of your choice) that launches a web browser and navigates to the WebdriverIO website
-// (http://webdriver.io).  Click on the 'API' link in the top navigation bar and load their API documentation (https://webdriver.io/docs/api).
-// Use the search functionality on this page to search their API documentation for the text 'Click'. Validate the correct information/page is returned.
-// Expand the “Protocols” section in the left navigation bar and have your test verify that the list under the Protocols section is correct.
-// Lastly, create another set of test cases that would further test the functionality of this search widget.
-// If possible use the Page Object model for element locators and helper functions.
+/*  UI Automation Exercise
+    Write a UI end to end user test class (language of your choice) that launches a web browser and navigates to the WebdriverIO website
+    (http://webdriver.io).  Click on the 'API' link in the top navigation bar and load their API documentation (https://webdriver.io/docs/api).
+    Use the search functionality on this page to search their API documentation for the text 'Click'. Validate the correct information/page is returned.
+    Expand the “Protocols” section in the left navigation bar and have your test verify that the list under the Protocols section is correct.
+    Lastly, create another set of test cases that would further test the functionality of this search widget.
+    If possible use the Page Object model for element locators and helper functions.
+*/
 
 
 require("chromedriver");
 var assert = require("assert");
 const config = require("../config.json");
+const test_data = require("../test_data.json");
 const homepage = require("../pageobjects/homepage");
 
 describe("testsuite for webdrive page", function () {
@@ -31,9 +33,9 @@ describe("testsuite for webdrive page", function () {
 
   it("test for search bar", async () => {
     let search = config.elements.searchbar.class;
-    assert.strictEqual(await homepage.getTextByClass(search), "Search");
+    assert.strictEqual(await homepage.getTextByClass(search), test_data.search_bar);
 
-    assert.strictEqual(await homepage.elementDisplay(search),true);
+    assert.strictEqual(await homepage.checkElementDisplay(search),true);
     
   });
 
@@ -42,23 +44,22 @@ describe("testsuite for webdrive page", function () {
     await homepage.clickOnElementByClass(search);
 
     let search_input = config.elements.search_input.id;
-    await homepage.sendKeys(search_input, "Click");
+    await homepage.sendKeys(search_input, test_data.search_input);
 
     let result_0 = config.elements.search_result_0.id;
-    assert.strictEqual(await homepage.getTextById(result_0), "click");
+    assert.strictEqual(await homepage.getTextById(result_0), test_data.search_result.result_1);
 
     let result_1 = config.elements.search_result_1.id;
-    assert.strictEqual(await homepage.getTextById(result_1), "Usage\nclick");
+    assert.strictEqual(await homepage.getTextById(result_1), test_data.search_result.result_2);
 
     let result_2 = config.elements.search_result_2.id;
-    assert.strictEqual(await homepage.getTextById(result_2), "doubleClick");
+    assert.strictEqual(await homepage.getTextById(result_2), test_data.search_result.result_3);
 
     let result_3 = config.elements.search_result_3.id;
-    assert.strictEqual(await homepage.getTextById(result_3), "isClickable");
+    assert.strictEqual(await homepage.getTextById(result_3), test_data.search_result.result_4);
 
     let result_4 = config.elements.search_result_4.id;
-    assert.strictEqual(await homepage.getTextById(result_4),"waitForClickable"
-    );
+    assert.strictEqual(await homepage.getTextById(result_4), test_data.search_result.result_5);
   });
 
   it("test for list of protocols", async () => {
@@ -67,25 +68,25 @@ describe("testsuite for webdrive page", function () {
     await homepage.driver.sleep(500);
     
     var protocols_list_0 = config.elements.protocols_list_0.xpath;
-    assert.strictEqual(await homepage.getTextByXpath(protocols_list_0), "WebDriver Protocol");
+    assert.strictEqual(await homepage.getTextByXpath(protocols_list_0), test_data.protocol_lists.result_1);
 
     var protocols_list_1 = config.elements.protocols_list_1.xpath;
-    assert.strictEqual(await homepage.getTextByXpath(protocols_list_1), "Appium");
+    assert.strictEqual(await homepage.getTextByXpath(protocols_list_1), test_data.protocol_lists.result_2);
 
     var protocols_list_2 = config.elements.protocols_list_2.xpath;
-    assert.strictEqual(await homepage.getTextByXpath(protocols_list_2), "Mobile JSON Wire Protocol");
+    assert.strictEqual(await homepage.getTextByXpath(protocols_list_2), test_data.protocol_lists.result_3);
 
     var protocols_list_3 = config.elements.protocols_list_3.xpath;
-    assert.strictEqual(await homepage.getTextByXpath(protocols_list_3), "Chromium");
+    assert.strictEqual(await homepage.getTextByXpath(protocols_list_3), test_data.protocol_lists.result_4);
 
     var protocols_list_4 = config.elements.protocols_list_4.xpath;
-    assert.strictEqual(await homepage.getTextByXpath(protocols_list_4), "Sauce Labs");
+    assert.strictEqual(await homepage.getTextByXpath(protocols_list_4), test_data.protocol_lists.result_5);
 
     var protocols_list_5 = config.elements.protocols_list_5.xpath;
-    assert.strictEqual(await homepage.getTextByXpath(protocols_list_5), "Selenium Standalone");
+    assert.strictEqual(await homepage.getTextByXpath(protocols_list_5), test_data.protocol_lists.result_6);
 
     var protocols_list_6 = config.elements.protocols_list_6.css;
-    assert.strictEqual(await homepage.getTextByCss(protocols_list_6), "JSON Wire Protocol");
+    assert.strictEqual(await homepage.getTextByCss(protocols_list_6), test_data.protocol_lists.result_7);
   
   });
 
